cf5fbbe16450e67e9dfbd9b95b0035bbe340dc48
Sandro Putraa <59155826+sandrocods@users.noreply.github.com>
2021-06-11 19:35:59 +0700

    Update example.php

--- example.php
+++ example.php
@@ -8,43 +8,69 @@ $GeneratorEmail = new GeneratorEmail();
  * Generate New Random Email
  */
 
-print_r($GeneratorEmail->getEmail());
+ print_r($GeneratorEmail->getEmail());
 
 /**
  * Generate New Email with specific Name & Domain
  */
 
- print_r($GeneratorEmail->getEmail('hraed.amale', 'boranora.com')); 
+print_r($GeneratorEmail->getEmail('mdiulyano.sinihun', 'defvit.com'));
 
 /**
  * Check Status Email
  */
- print_r($GeneratorEmail->CheckValidation()); 
-
-
+print_r($GeneratorEmail->CheckValidation());
 
 /**
- * Read All Messages in Inbox
+ * Get Data Inbox Convert to array
  */
-print_r($GeneratorEmail->ReadEmail());
+print_r($GeneratorEmail->ReadEmail()->getData());
+
+/**
+ * Regex String in body message , Only Works In 1 Messages
+ * Return All Regex by your pattren
+ */
+
+print_r($GeneratorEmail->ReadEmail()->GetSpecific('/OTP : (.*)/m'));
 
 
 /**
  * Delete All Email in Inbox
  */
-print_r($GeneratorEmail->DeleteAll($GeneratorEmail->ReadEmail()['Dell_key']));
+
+$GeneratorEmail->DeleteAll($GeneratorEmail->ReadEmail()->getData()['Dell_key']);
 
 /**
- * Read Body Messages by specific Message
+ * Read Specific only works when Inbox have 2 email
+ * if you read one message only use $GeneratorEmail->getEmail('user', 'domain')->ReadEmail()->getData()
  */
 
-foreach ($GeneratorEmail->ReadEmail()['Data'] as $key => $value) {
-    print_r($GeneratorEmail->ReadSpecific($value['LongAddress']));
+if ($GeneratorEmail->ReadEmail()->getData()['Count_message'] > 1) {
 
     /**
-     * Delete Message by specific Message
+     * Read Body Messages by specific Message
      */
-    print_r($GeneratorEmail->DeleteMessage($GeneratorEmail->ReadSpecific($value['LongAddress'])['Dell_key'], $value['LongAddress']));
 
-    echo "\n";
+    foreach ($GeneratorEmail->ReadEmail()->getData()['Data'] as $key => $value) {
+
+        print_r($GeneratorEmail->ReadSpecific($value['LongAddress']));
+
+        /**
+         * Read Specific only works when Inbox have 2 email
+         * if you read one message only use $GeneratorEmail->getEmail('user', 'domain')->ReadEmail()->getData()
+         */
+        print_r($GeneratorEmail->ReadSpecific($value['LongAddress'])->GetSpecific('/OTP : (.*)/m'));
+
+        /**
+         * Delete Message by specific Message
+         */
+        print_r($GeneratorEmail->DeleteMessage($GeneratorEmail->ReadSpecific($value['LongAddress'])->getData()['Dell_key'], $value['LongAddress']));
+
+        echo "\n";
+    }
+
+} else {
+
+    print_r($GeneratorEmail->ReadEmail()->getData());
+
 }
